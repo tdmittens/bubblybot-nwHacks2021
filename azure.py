4cabc7697d6ac6c7adc62004ae23944d022cd762
Tarandeep <tmittal@ryerson.ca>
2021-01-09 22:37:46 -0500

    updates to bot/azure api

--- azure.py
+++ azure.py
@@ -2,7 +2,7 @@ from azure.ai.textanalytics import TextAnalyticsClient
 from azure.core.credentials import AzureKeyCredential
 from discord import client
 
-key = "e06e0622f209415e9b2b215c7b09015f"
+key = "85c6af76f9284d2b90f54e1e3317eed5"
 endpoint = "https://nwhacks2021sentiment.cognitiveservices.azure.com/"
 
 
@@ -14,12 +14,30 @@ def authenticate_client():
     return text_analytics_client
 
 
-client = authenticate_client()
+# azure_client = authenticate_client()
+
+
+def sentiment_analysis(client, statement):
+
+    # array of strings - response = client.analyze_sentiment(documents=documents)
+    # need to divide array of strings to one string at a time
+    # this will determine what sentiment is - <string>.sentiment
+    # sentence.confidence_scores.positive
+    # sentence.confidence_scores.neutral
+    # sentence.confidence_scores.negative
+    response = client.analyze_sentiment(documents=statement)[0]
+    return(response.sentiment)
+
+    # array of strings - response = client.analyze_sentiment(documents=documents)
+    # need to divide array of strings to one string at a time
+    # this will determine what sentiment is - <string>.sentiment
+    # sentence.confidence_scores.positive
+    # sentence.confidence_scores.neutral
+    # sentence.confidence_scores.negative
 
 
 def sentiment_analysis_example(client):
 
-    # this will determine what sentiment is - <string>.sentiment
     documents = [
         "I had the best day of my life. I wish you were there with me."]
     response = client.analyze_sentiment(documents=documents)[0]
@@ -39,4 +57,4 @@ def sentiment_analysis_example(client):
         ))
 
 
-sentiment_analysis_example(client)
+# sentiment_analysis_example(azure_client)
--- main.py
+++ main.py
@@ -7,17 +7,26 @@ from dotenv import load_dotenv
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot
 from itertools import cycle
+import json
 import random
 
+from azure import sentiment_analysis, authenticate_client
+
+# Azure Init
+azure_client = authenticate_client()
+
+load_dotenv()
 # client = authenticate_client()
 # Confirming the name of my chosen server, if we want it for something specific.
 GUILD = os.getenv('nwHacks Bot Server')
 client = commands.Bot(command_prefix="~")  # This bot uses ~ to do commands.
-token = 'Nzk3NTg3ODY5MTE5Njc2NDI3.X_oplg.S_88iTk6qPw1MKfEpz7NNOl9T3Y'
+token = ''
 
 # -------------------
 # On Ready Commands
 # -------------------
+
+print('test')
 
 
 @client.event
@@ -30,6 +39,129 @@ async def on_ready():
             break
 
     print(
-        f'{client.user} is connected to the following guild:\n'
+        f'{client.user} is connec ted to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+# -------
+# TASKS
+# -------
+botStatus1 = cycle(
+    ['with your heart.', 'with your mind', 'with your feelings'])
+
+
+@tasks.loop(seconds=30)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(botStatus1)))
+# ------------------
+# RANDOM COMMANDS
+# ------------------
+client.remove_command('help')
+
+
+@client.command()
+async def help(ctx):  # prettier version of help? Maybe later.
+    author = ctx.message.author
+    embed = discord.Embed(
+        colour=discord.Colour.orange()
+    )
+    embed = discord.Embed(title="------\nHELP\n------", color=0xff40ff)
+    embed.add_field(name='~leaderboard',
+                    value='Create custom channel for sentiment scoring', inline=False)
+    embed.add_field(name='~score @name',
+                    value='Returns the score of a specific server member', inline=False)
+    embed.add_field(name='~score @role',
+                    value='Returns the score of a specific server role', inline=False)
+    embed.add_field(
+        name='~ping', value='Return current bot latency', inline=False)
+    embed.add_field(
+        name='~clear #', value='Clear messages from current channel, where # = number of messages you wish to delete', inline=False)
+    await ctx.send(embed=embed)
+
+
+@client.command()  # Find the current latency of the bot
+async def ping(ctx):
+    # latency in milliseconds
+    await ctx.send(f'Bot ping is {round(client.latency * 1000)} ms')
+
+
+@client.command()  # text
+async def niceMsg(ctx):
+    await ctx.send(f'Annie & Ella are amazing and Johnny is not.')
+
+
+@client.command()  # clear a channels messages.
+@commands.has_permissions(manage_messages=True)
+async def clear(ctx, amount=1):  # default 1 message if not specified
+    await ctx.channel.purge(limit=amount+1)
+    await ctx.send(f'**[ {amount} ] messages have been yeeted.**')
+    time.sleep(2)  # 5 second delay for message above.
+    await ctx.channel.purge(limit=1)
+
+# -------------
+# ON MESSAGE
+# -------------
+
+
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+    if message.author == client.user:
+        return
+
+    if message.content == "type?":
+        response = str(message)
+        response = type(response)
+        await message.channel.send(response)
+
+    if message.content.lower() == 'what team?':
+        response = 'Wildcats!'
+        await message.channel.send(response)
+
+    if message.content == 'Hi':
+        response = 'Hey'
+        await message.channel.send(response)
+
+    if message.content == 'Annie hates Johnny.':
+        statement = [str(message.content)]
+        response = sentiment_analysis(azure_client, statement)
+        await message.channel.send(response)
+
+
+def new_func(message):
+    message = message.lower()
+    return message
+
+# -----------------
+# ERROR HANDLING
+# -----------------
+
+
+@ client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('You did not use a valid command, silly billy.')
+
+# -------------
+# VOICE
+# -------------
+
+
+@ client.command()
+async def joinvoice(ctx):
+    connected = ctx.author.voice
+    if connected:
+        await connected.channel.connect()
+
+
+@ client.command(pass_context=True)
+async def leavevoice(ctx):
+    connected = ctx.author.voice
+    if connected:
+        await connected.channel.disconnect()
+
+
+# -------------
+# RUN BOT
+# -------------
+client.run(token)
